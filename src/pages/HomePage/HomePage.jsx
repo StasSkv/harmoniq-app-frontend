27fec34bdd7fd25d8399135846ab7f4cdb5dc045
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-25 22:20:28 +0200

    Create HomePage with main layout and components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,21 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Hero from "../../components/Hero/Hero";
+import About from "../../components/About/About";
+import PopularArticles from "../../components/PopularArticles/PopularArticles";
+import Creators from "../../components/Creators/Creators";
+
 export const HomePage = () => {
-  return <p>HomePage</p>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <PopularArticles />
+        <Creators />
+      </main>
+      <Footer />
+    </>
+  );
 };
