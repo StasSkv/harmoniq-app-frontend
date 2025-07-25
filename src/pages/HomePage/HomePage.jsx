8327dd9828f7e0f0fd8c353643bd94343d539a99
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-25 22:14:23 +0200

    loader could be done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,3 @@
-import { useDispatch } from 'react-redux';
-import { setIsLoading } from '../../redux/global/globalSlice';
-
-const HomePage = () => {
-  const dispatch = useDispatch();
-
-  const handleTestLoader = () => {
-    dispatch(setIsLoading(true));
-
-    // Симуляція затримки (наприклад, фетч з сервера)
-    setTimeout(() => {
-      dispatch(setIsLoading(false));
-    }, 2000);
-  };
-
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Home Page</h1>
-      <button onClick={handleTestLoader}>Перевірити Loader</button>
-    </div>
-  );
+export const HomePage = () => {
+  return <p>HomePage</p>;
 };
-
-export default HomePage;
