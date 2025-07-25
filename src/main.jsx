622e005538fdb04904a08bf7c22f00aaff800789
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-25 22:00:09 +0200

    TEMP: Додано Loader в Main для тесту

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-import "modern-normalize";
-import "./styles/index.css";
+// import "modern-normalize";
+// import "./styles/index.css";
 
-import App from "./components/App/App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+// import App from "./components/App/App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from '../../redux/global/globalSelectors';
+import Loader from '../Loader/Loader'; // шляхи можуть відрізнятися
+
+const Main = () => {
+  const isLoading = useSelector(selectIsLoading);
+
+  return (
+    <main style={{ minHeight: '100vh', position: 'relative' }}>
+      {isLoading && <Loader />}
+      
+      {/* Тут буде твій реальний вміст */}
+      <h2>Це Main</h2>
+    </main>
+  );
+};
+
+export default Main;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,24 @@
-export const HomePage = () => {
-  return <p>HomePage</p>;
+import { useDispatch } from 'react-redux';
+import { setIsLoading } from '../../redux/global/globalSlice';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const handleTestLoader = () => {
+    dispatch(setIsLoading(true));
+
+    // Симуляція затримки (наприклад, фетч з сервера)
+    setTimeout(() => {
+      dispatch(setIsLoading(false));
+    }, 2000);
+  };
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Home Page</h1>
+      <button onClick={handleTestLoader}>Перевірити Loader</button>
+    </div>
+  );
 };
+
+export default HomePage;
