6bb8af682b63aa6b6fe857c764fbf811b8c3d189
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-26 17:40:24 +0200

    add getCurrentUser operation and protected routes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import articlesReducer from "./articlesSlice/articlesSlice";
+import authReducer from "./auth/authSlice"; 
 
 export const store = configureStore({
   reducer: {
     articles: articlesReducer,
+    auth: authReducer,
   },
 });
