f132556a438ba2703c8bbcbf4511d8d889fca008
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-26 12:36:48 +0200

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import articlesReducer from "./articlesSlice/articlesSlice";
+import authReducer from "../redux/auth/authSlice"
 
 export const store = configureStore({
   reducer: {
     articles: articlesReducer,
+    auth: authReducer,
   },
 });
