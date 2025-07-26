9546853cd5b1bdc691fdd5af26dbe1f635989dfb
lozovskaadasa9@gmail.com <lozovskaadasa9@gmail.com>
2025-07-26 16:32:56 +0200

    done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,3 @@
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
 import Hero from "../../components/Hero/Hero";
 import About from "../../components/About/About";
 import PopularArticles from "../../components/PopularArticles/PopularArticles";
@@ -8,14 +6,12 @@ import Creators from "../../components/Creators/Creators";
 export const HomePage = () => {
   return (
     <>
-      <Header />
       <main>
         <Hero />
         <About />
         <PopularArticles />
         <Creators />
       </main>
-      <Footer />
     </>
   );
 };
