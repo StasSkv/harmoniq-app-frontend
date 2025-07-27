6c97928fc78f6d54fe3a480fbcbbf305a62a9073
Len4ik <karimovaolena@yahoo.com>
2025-07-27 16:38:58 +0300

    headerUnauth navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,16 +8,6 @@ import { AuthorsPage } from '../../pages/AuthorsPage/AuthorsPage.jsx';
 import { CreateArticlePage } from '../../pages/CreateArticlePage/CreateArticlePage.jsx';
 import { LoginPage } from '../../pages/LoginPage/LoginPage.jsx';
 import { RegisterPage } from '../../pages/RegisterPage/RegisterPage.jsx';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from '../Loyout/Loyout.jsx';
-import { HomePage } from '../../pages/HomePage/HomePage.jsx';
-import { ArticlePage } from '../../pages/ArticlePage/ArticlePage.jsx';
-import { ArticlesPage } from '../../pages/ArticlesPage/ArticlesPage.jsx';
-import { AuthorProfilePage } from '../../pages/AuthorProfilePage/AuthorProfilePage.jsx';
-import { AuthorsPage } from '../../pages/AuthorsPage/AuthorsPage.jsx';
-import { CreateArticlePage } from '../../pages/CreateArticlePage/CreateArticlePage.jsx';
-import { LoginPage } from '../../pages/LoginPage/LoginPage.jsx';
-import { RegisterPage } from '../../pages/RegisterPage/RegisterPage.jsx';
 import { CreatorsPage } from '../../pages/CreatorsPage/CreatorsPage.jsx';
 
 export const App = () => {
