5a0d425a83d5ed4c836769cb0ec4dc01e4b0f6aa
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-07-27 22:27:19 +0300

    fixed App.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,9 @@
-import { LoginPage } from '../../pages/LoginPage/LoginPage';
-import { RegisterPage } from '../../pages/RegisterPage/RegisterPage';
 import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <div>
       <p>Header</p>
-      <NavLink to="/loginPage">Login</NavLink>
-      <NavLink to="/registerPage">Register</NavLink>
     </div>
   );
 };
