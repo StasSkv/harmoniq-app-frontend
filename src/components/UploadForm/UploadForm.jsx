d66e0ded1867d033df6c21b93cb0a42df74d809c
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-07-27 22:08:35 +0300

    fixed UploadPhoto and Registration

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -2,12 +2,13 @@ import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { registerThunk } from '../../redux/auth/operations';
+import sprite from '../../assets/icons/sprite.svg';
 import { clearRegistrationData } from '../../redux/auth/registrationSlice';
 import * as Yup from 'yup';
 import { toast } from 'react-toastify';
 import { useState } from 'react';
-
 import s from './UploadForm.module.css';
+import { Container } from '../Container/Container';
 
 const UploadPhotoForm = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,7 @@ const UploadPhotoForm = () => {
   const { name, email, password } = useSelector((state) => state.registration);
   const [preview, setPreview] = useState(null);
 
+  console.log(name);
   const formik = useFormik({
     initialValues: {
       photo: null,
@@ -30,6 +32,7 @@ const UploadPhotoForm = () => {
       formData.append('avatar', values.photo);
 
       try {
+        console.log(formData);
         await dispatch(registerThunk(formData)).unwrap();
         dispatch(clearRegistrationData());
         navigate('/');
@@ -53,37 +56,39 @@ const UploadPhotoForm = () => {
   };
 
   return (
-    <form onSubmit={formik.handleSubmit} className={s.form}>
-      <h3 className={s.title}>Upload your photo</h3>
+    <Container>
+      <form onSubmit={formik.handleSubmit} className={s.form}>
+        <h3 className={s.title}>Upload your photo</h3>
 
-      <div className={s.avatarWrapper}>
-        <label htmlFor="photo" className={s.avatarLabel}>
-          {preview ? (
-            <img src={preview} alt="Preview" className={s.avatarImage} />
-          ) : (
-            <svg width="24" height="24">
-              <use href="../../assets/icons/sprite.svg#icon-user" />
-            </svg>
-          )}
-        </label>
-        <input
-          id="photo"
-          type="file"
-          accept="image/*"
-          name="photo"
-          onChange={handleFileChange}
-          className={s.inputHidden}
-        />
-      </div>
+        <div className={s.avatarWrapper}>
+          <label htmlFor="photo" className={s.avatarLabel}>
+            {preview ? (
+              <img src={preview} alt="Preview" className={s.avatarImage} />
+            ) : (
+              <svg className={s.cameraIcon} width="60" height="60">
+                <use href={`${sprite}#icon-user`} />
+              </svg>
+            )}
+          </label>
+          <input
+            id="photo"
+            type="file"
+            accept="image/*"
+            name="photo"
+            onChange={handleFileChange}
+            className={s.inputHidden}
+          />
+        </div>
 
-      {formik.touched.photo && formik.errors.photo && (
-        <div className={s.error}>{formik.errors.photo}</div>
-      )}
+        {formik.touched.photo && formik.errors.photo && (
+          <div className={s.error}>{formik.errors.photo}</div>
+        )}
 
-      <button type="submit" className={s.submitBtn}>
-        Save
-      </button>
-    </form>
+        <button type="submit" className={s.submitBtn} disabled={!formik.values.photo}>
+          Save
+        </button>
+      </form>
+    </Container>
   );
 };
 
