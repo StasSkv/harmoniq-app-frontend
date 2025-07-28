623eb2275e0a2a15d4a2bdec87de597f7cabf9ac
Alekseii Levchenko <aleksej.levchenko93@gmail.com>
2025-07-28 19:50:46 +0300

    reverted changes to HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,3 @@
-// export const HomePage = () => {
-//   return <p>HomePage</p>;
-// };
-
-import Hero from '../../components/Hero/Hero';
-
 export const HomePage = () => {
-  return (
-    <main>
-      <Hero />
-    </main>
-  );
+  return <p>HomePage</p>;
 };
