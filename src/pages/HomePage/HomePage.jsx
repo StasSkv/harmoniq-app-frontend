ec3ecd13ce80f11ff3be416d93d8b0ee7673b9c4
Alekseii Levchenko <aleksej.levchenko93@gmail.com>
2025-07-28 19:19:17 +0300

    correct styles, correct mobile photo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,13 @@
+// export const HomePage = () => {
+//   return <p>HomePage</p>;
+// };
+
+import Hero from '../../components/Hero/Hero';
+
 export const HomePage = () => {
-  return <p>HomePage</p>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 };
