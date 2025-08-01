a3542aa2c6d36beff3732ef6c10dd952e7a8b2e5
Nevskiy911 <maleev1820@gmail.com>
2025-08-01 21:14:07 +0200

    fixed code

--- src/pages/ArticlePage/ArticlePage.jsx
+++ src/pages/ArticlePage/ArticlePage.jsx
@@ -5,15 +5,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchArticleById } from '../../redux/articlesSlice/articlesOperation';
 import { LoaderPage } from '../../components/Loader/LoaderPage/LoaderPage';
 import { Container } from '../../components/Container/Container';
+import DOMPurify from 'dompurify';
+import {
+  selectCurrentArticle,
+  selectError,
+  selectIsLoading,
+} from '../../redux/articlesSlice/articlesSelectors';
 
 const ArticlePage = () => {
   const { articleId } = useParams();
   const dispatch = useDispatch();
 
-  const rawArticle = useSelector((state) => state.articles.currentArticle);
-  const article = rawArticle?.data || rawArticle;
-  const isLoading = useSelector((state) => state.articles.isLoading);
-  const error = useSelector((state) => state.articles.error);
+  const currentArticle = useSelector(selectCurrentArticle);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const isHTML = (str) => /<\/?[a-z][\s\S]*>/i.test(str);
+  const article = currentArticle?.data || currentArticle;
 
   useEffect(() => {
     dispatch(fetchArticleById(articleId));
@@ -30,10 +38,15 @@ const ArticlePage = () => {
         {article.img && <img src={article.img} alt={article.title} className={s.articleImg} />}
         <div className={s.content}>
           <div className={s.articleText}>
-            {' '}
-            {article.article.split('\n').map((paragraph, idx) => (
-              <p key={idx}>{paragraph}</p>
-            ))}
+            {isHTML(article.article) ? (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(article.article),
+                }}
+              />
+            ) : (
+              article.article.split('\n').map((paragraph, idx) => <p key={idx}>{paragraph}</p>)
+            )}
           </div>
           <div className={s.future}>You can also interested</div>
         </div>
