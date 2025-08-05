7957169d6d4af64bfa75a8b1fb55a9033cb913d3
Nevskiy911 <maleev1820@gmail.com>
2025-08-05 22:32:28 +0200

    first steps in del the articles

--- src/components/ArticleItem/ArticleItem.jsx
+++ src/components/ArticleItem/ArticleItem.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import s from './ArticleItem.module.css';
 import ButtonAddToBookmarks from '../ButtonAddToBookmarks/ButtonAddToBookmarks.jsx';
 import DOMPurify from 'dompurify';
 
 export const ArticleItem = ({ img, title, article, ownerName, articleId, ownerId }) => {
+  const location = useLocation(); // <--- додано хук
+
   const isHTML = (str) => /<\/?[a-z][\s\S]*>/i.test(str);
 
   return (
     <div className={s.article}>
-      <Link to={`/articles/${articleId}`}>
+      <Link to={`/articles/${articleId}`} state={{ from: location.pathname + location.search }}>
         <img src={img} alt="Article" className={s.image} />
       </Link>
       <div className={s.articleInfoWrapper}>
@@ -29,7 +31,11 @@ export const ArticleItem = ({ img, title, article, ownerName, articleId, ownerId
         </div>
       </div>
       <div className={s.buttons}>
-        <Link className={s.learnLink} to={`/articles/${articleId}`}>
+        <Link
+          className={s.learnLink}
+          to={`/articles/${articleId}`}
+          state={{ from: location.pathname + location.search }}
+        >
           Learn more
         </Link>
         <ButtonAddToBookmarks articleId={articleId} ownerId={ownerId} />
--- src/pages/ArticlePage/ArticlePage.jsx
+++ src/pages/ArticlePage/ArticlePage.jsx
@@ -1,8 +1,13 @@
 import s from './ArticlePage.module.css';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchArticleById } from '../../redux/articlesSlice/articlesOperation';
+import {
+  fetchArticleById,
+  deleteArticle,
+  fetchAllArticles,
+  fetchArticlesWithParams,
+} from '../../redux/articlesSlice/articlesOperation';
 import { LoaderPage } from '../../components/Loader/LoaderPage/LoaderPage';
 import { Container } from '../../components/Container/Container';
 import DOMPurify from 'dompurify';
@@ -11,11 +16,14 @@ import {
   selectError,
   selectIsLoading,
   selectArticles,
+  selectUser,
 } from '../../redux/articlesSlice/articlesSelectors';
-import { fetchAllArticles } from '../../redux/articlesSlice/articlesOperation';
 import { RecommendedArticles } from '../../components/RecommendedArticles/RecommendedArticles';
 
 const ArticlePage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const { articleId } = useParams();
   const dispatch = useDispatch();
   const recommended = useSelector(selectArticles);
@@ -27,6 +35,12 @@ const ArticlePage = () => {
   const isHTML = (str) => /<\/?[a-z][\s\S]*>/i.test(str);
   const article = currentArticle?.data || currentArticle;
 
+  const user = useSelector(selectUser);
+  const from = location.state?.from || '/articles?page=1&filter=all';
+  const urlParams = new URLSearchParams(from.split('?')[1]);
+  const page = Number(urlParams.get('page')) || 1;
+  const filter = urlParams.get('filter') || 'all';
+
   useEffect(() => {
     dispatch(fetchAllArticles({ filter: 'popular', limit: 3 }));
     dispatch(fetchArticleById(articleId));
@@ -35,6 +49,19 @@ const ArticlePage = () => {
   if (isLoading) return <LoaderPage />;
   if (error) return <p>Помилка: {error}</p>;
   if (!article) return <p>Статтю не знайдено</p>;
+
+  const handleDelete = () => {
+    const confirmed = window.confirm('Ви впевнені, що хочете видалити цю статтю?');
+    if (confirmed) {
+      dispatch(deleteArticle(articleId))
+        .unwrap()
+        .then(() => {
+          dispatch(fetchArticlesWithParams({ page, filter, limit: 3 }));
+          navigate(from);
+        })
+        .catch((err) => console.error('Помилка видалення:', err));
+    }
+  };
 
   return (
     <article className={s.articlePage}>
@@ -54,11 +81,13 @@ const ArticlePage = () => {
             ) : (
               article.article.split('\n').map((paragraph, idx) => <p key={idx}>{paragraph}</p>)
             )}
+            {user && article?.ownerId && user._id === article.ownerId && (
+              <button onClick={handleDelete} className={s.deleteBtn}>
+                Видалити статтю
+              </button>
+            )}
           </div>
-          <RecommendedArticles
-            currentArticle={currentArticle}
-            recommended={recommended}
-          />
+          <RecommendedArticles currentArticle={currentArticle} recommended={recommended} />
         </div>
       </Container>
     </article>
